09a8a702c6623ffbe6dde256d742ced9456ed40f
Patrick Han <patric1khan@gmail.com>
2020-09-07 14:45:27 -0500

    Implemented prethreading
    Currently can handle multiple clients, but messages from other clients will only show up the server

--- chat_server.c
+++ chat_server.c
@@ -18,14 +18,90 @@
 #define NTHREADS 4		// Number of worker threads
 
 
+// Typedefs
+typedef struct {
+	int *buf;		/* Buffer array */
+	int n;			/* Maximum number of slots */
+	int front;		/* buf[(front+1)%n] is the first tiem */
+	int rear;		/* buf[rear%n] is the last item */
+	int slots;		/* Counts avalible slots */
+	pthread_mutex_t mutex;	/* Protects access to buf */
+	pthread_cond_t not_empty;	
+	pthread_cond_t not_full;
+} sbuf_t;
+
+
 
 // Function definitions
 void doit(int connfd);
 void *thread(void *vargp);
 
 
+/*
+ * Requires:
+ *   Desired size of the bounded buffer and a pointer to the sbuf_t pointer.
+ *
+ * Effects:
+ *   Initializes a bounded buffer for holding connection requests.
+ */
+static void 
+sbuf_init(sbuf_t *sp, int n)
+{
+	// sp->buf = Calloc(n, sizeof(int));
+    sp->buf = calloc(n, sizeof(int));
+	sp->n = n;				/* Buffer holds n items */
+	sp->front = sp->rear = 0;		/* Empty iff front == rear */
+	pthread_mutex_init(&sp->mutex, NULL);	/* Default mutex for locking */
+	sp->slots = 0;				/* All slots available */
+	pthread_cond_init(&sp->not_full, NULL);	
+	pthread_cond_init(&sp->not_empty, NULL);
+}
 
-// sbuf_t sbuf; // Shared buffer of connected descriptors
+/* 
+ * Requires:
+ *   Buffer pointer and item to be inserted.
+ *
+ * Effects:
+ *   Inserts item into the rear of the buffer.
+ */
+static void 
+sbuf_insert(sbuf_t *sp, int item)
+{				
+	pthread_mutex_lock(&sp->mutex);		/* Lock the buffer */
+	while (sp->slots == sp->n) {		/* Wait for available slot */
+		pthread_cond_wait(&sp->not_full, &sp->mutex);
+	}
+	sp->buf[(++sp->rear) % (sp->n)] = item;	/* Insert the item */
+	sp->slots = sp->slots + 1;
+	pthread_cond_signal(&sp->not_empty);
+	pthread_mutex_unlock(&sp->mutex);	/* Unlock the buffer */
+			
+}
+
+/* 
+ * Requires:
+ *   Buffer pointer.
+ *
+ * Effects:
+ *   Removes first item from buffer.
+ */
+static int 
+sbuf_remove(sbuf_t *sp)
+{
+	int item;
+	pthread_mutex_lock(&sp->mutex);		/* Lock the buffer */
+	while (sp->slots == 0) {		/* Wait for available item */
+		pthread_cond_wait(&sp->not_empty, &sp->mutex);
+	}			
+	item = sp->buf[(++sp->front) % (sp->n)];/* Remove the item */
+	sp->slots = sp->slots - 1;
+	pthread_cond_signal(&sp->not_full);	/* Announce available slot */
+	pthread_mutex_unlock(&sp->mutex);	/* Unlock the buffer */
+	return item;
+}
+
+
+sbuf_t sbuf; // Shared buffer of connected descriptors
 
 int main(int argc, char **argv)
 {
@@ -50,6 +126,10 @@ int main(int argc, char **argv)
 
     // Ignore SIGPIPE
 	signal(SIGPIPE, SIG_IGN);
+
+
+
+
 
     /* Setup server */
 
@@ -89,11 +169,32 @@ int main(int argc, char **argv)
         exit(EXIT_FAILURE);
     }
 
-     int connfd = 0;
-     struct sockaddr_in client_address;
 
 
-    // Handle clients
+
+
+
+
+     
+    /* Pre-threading setup */
+    pthread_t tid;
+    
+	sbuf_init(&sbuf, SBUFSIZE); // Create bounded buffer for our worker threads
+	for (int i = 0; i < NTHREADS; i++) // Spawn worker threads
+    {
+        // Pthread_create(&tid, NULL, thread, NULL);
+		pthread_create(&tid, NULL, thread, NULL);
+    }
+
+
+
+
+
+    /* Client settings */
+    int connfd = 0;
+    struct sockaddr_in client_address;
+
+    /* Handle clients */
     while(1)
     {
         // Accept a connection
@@ -104,7 +205,7 @@ int main(int argc, char **argv)
         // }
 
 
-        // Create a conneted descriptor that can be used to communicate with the client
+        // Create a connected descriptor that can be used to communicate with the client
         socklen_t client_addr_len = sizeof(client_address);
         if ((connfd = accept(serverfd, (struct sockaddr *)&client_address, &client_addr_len)) < 0) 
         { 
@@ -113,14 +214,53 @@ int main(int argc, char **argv)
         }
 
         // Handle the client
-        doit(connfd);
+        // doit(connfd);
+
+        // Insert the connfd into the bounded buffer (Done by the main thread)
+        sbuf_insert(&sbuf, connfd);
     }
 
     return(0);
 }
 
+/* 
+ * Requires:
+ *   Nothing
+ *
+ * Effects:
+ *   Services client request with worker thread
+ */
+void *thread(void *vargp)
+{
+	(void) vargp; // Avoid message about unused arguments
+	// Pthread_detach(pthread_self()); // No return values
+    pthread_detach(pthread_self()); // No return values
+	while(1) {
+		    // Remove descriptor from bounded buffer
+		    int connfd = sbuf_remove(&sbuf);
+        	struct sockaddr_in addr;
+        	socklen_t addr_size = sizeof(struct sockaddr_in);
+        	int res = getpeername(connfd, (struct sockaddr *)&addr, &addr_size);
+        	if (res < 0) {
+           		// Error
+            	// printf("Error with getting peer name from file descriptor %d, %s\n", res, strerror(errno));
+                printf("Error with getting peer name from file descriptor %d\n", res);
+	    		continue;
+        	}
+		// Service client and close
+		// doit(connfd, &addr);
+        // Close(connfd);
+        doit(connfd);
+		close(connfd);
+	}
+}
 
-// doit() is for handling a single client, should move all server stuff out of it
+
+
+
+
+
+// doit() is for handling a single client
 
 void doit(int connfd)
 {
@@ -137,7 +277,4 @@ void doit(int connfd)
         printf("[Client] %s\n", buffer); // Print the client message on the server side
     }
     
-
-    // Close the connection
-    close(connfd);
 }
