449fc868a7dc6d40f51f99fb5343965c46a57f5b
Patrick Han <patric1khan@gmail.com>
2020-10-15 23:12:12 -0700

    Error handling for malloc() call in concat()

--- chat_server.c
+++ chat_server.c
@@ -201,7 +201,11 @@ client_struct* sbuf_remove(sbuf_t *sp)
 char* concat(const char *s1, const char *s2)
 {
     char *result = malloc(strlen(s1) + strlen(s2) + 1); // +1 for the null-terminator
-    // in real code you would check for errors in malloc here
+    if (result == NULL)
+    {
+        printf("concat(): failed to allocate memory.\n");
+        exit(EXIT_FAILURE);
+    }
     strcpy(result, s1);
     strcat(result, s2);
     return result;
